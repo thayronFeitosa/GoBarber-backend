eb0e58d09ddfd51fc625764c2de581c80acf9d13
thayron <thayronf3@gmail.com>
2020-11-25 06:33:36 -0300

    Criado o hash para criptogravar a senha do cliente

--- src/routes/Users.routes.ts
+++ src/routes/Users.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
-import CreateUserService from '../service/CreateUserService';
-
+import CreateUserService from '../services/CreateUserService';
+import {hash} from 'bcryptjs'
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
   try {
     const {name, email, password} = request.body;
     const createUser = new CreateUserService();
-    const user = await createUser.execute({name, email, password});
-    response.status(201).json(user)
+    const hashedPassword = await hash(password, 8)
+    const user = await createUser.execute({name, email, password:hashedPassword});
+
+    return response.json({ ...user, password: undefined });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
