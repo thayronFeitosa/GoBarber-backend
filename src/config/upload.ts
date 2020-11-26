68fd0192cf4f371c837d136f535fa62c8882a12d
thayron <thayronf3@gmail.com>
2020-11-26 07:27:20 -0300

    Criando um localstorage pra guardar as imagens de upload do usuario

---
+++ src/config/upload.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+import multer from 'multer';
+import crypto from 'crypto';
+import { request } from 'express';
+
+export default {
+  storage: multer.diskStorage({
+		destination: path.resolve(__dirname, '..', '..', 'tmp'),
+		filename(request, file, callback) {
+			const fileHash = crypto.randomBytes(10).toString('hex');
+			const fileName = `${fileHash}-${file.originalname}`;
+
+			return callback(null, fileName);
+		}
+	}),
+}
--- src/routes/Users.routes.ts
+++ src/routes/Users.routes.ts
@@ -1,19 +1,28 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
+
 
 usersRouter.post('/', async (request, response) => {
   try {
-    const {name, email, password} = request.body;
+    const { name, email, password } = request.body;
     const createUser = new CreateUserService();
 
-    const user = await createUser.execute({name, email, password});
+    const user = await createUser.execute({ name, email, password });
 
     return response.json({ ...user, password: undefined });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  return response.json({ ok: true });
+})
 
 export default usersRouter;
