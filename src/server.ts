8613cd30376ee56f7fc2f62335ae70d2d366d721
thayron <thayronf3@gmail.com>
2020-11-28 13:38:27 -0300

    Foi criado a rota para buscar o avatar do usuario

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes/index';
+import uploadConfig from './config/upload';
+
 import './database';
+
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+
 app.listen(3333, () => {
     console.log("servidor rodando na porta 3333")
 });
